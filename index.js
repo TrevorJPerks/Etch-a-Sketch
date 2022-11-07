80d9f52b1e6e7465d308e0b8475c65292a70fc3e
TrevorJPerks <TrevorJPerks@gmail.com>
2022-11-07 13:02:30 -0600

    CreateGrid(sliderValue)

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('we linked');
+let sliderValue = 16;
+
+function createGrid(sliderValue) {
+  const drawingArea = document.getElementById('etch-a-sketch');
+  // Remove all nodes before adding more. (Should probably have condition)
+  while (drawingArea.firstChild) {
+    drawingArea.removeChild(drawingArea.firstChild);
+  }
+
+  const fragment = document.createDocumentFragment();
+  const numberOfDivs = sliderValue ** 2;
+  const divSize = 600 / sliderValue;
+
+  for (i = 0; i < numberOfDivs; i++) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixel.style.height = `${divSize}px`;
+    pixel.style.width = `${divSize}px`;
+
+    fragment.appendChild(pixel);
+  }
+  drawingArea.appendChild(fragment);
+}
