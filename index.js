bf345d265d3f32ed6f87bf5f3a8d4dd9b78b9e45
TrevorJPerks <TrevorJPerks@gmail.com>
2022-11-08 08:27:48 -0600

    create paintEmptySquares(userColorSelection)

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const slider = document.querySelector('.slider');
-const sliderstep = (document.querySelector('.slider').step = '16');
-const output = document.querySelector('.slider-value');
-
-slider.oninput = () => {
-  output.innerHTML = `${slider.value} x ${slider.value} `;
-  createGrid(slider.value);
-};
-
-const sliderValue = slider.value;
-
 function createGrid(sliderValue) {
   const drawingArea = document.getElementById('etch-a-sketch');
+  sliderValue = slider.value;
   // Remove all nodes before adding more. (Should probably have condition)
   while (drawingArea.firstChild) {
     drawingArea.removeChild(drawingArea.firstChild);
   }
 
   const fragment = document.createDocumentFragment();
+  // Quick maths
   const numberOfDivs = sliderValue ** 2;
   const divSize = 600 / sliderValue;
 
@@ -31,15 +22,50 @@ function createGrid(sliderValue) {
   drawingArea.appendChild(fragment);
 }
 
-function clearCanvas() {
-  const block = document.querySelectorAll('.pixel');
+// Color of paintEmptySquares
+let userColorSelection = 'black';
 
-  block.forEach(function (pixel) {
-    pixel.style.backgroundColor = 'white';
+function paintEmptySquares(userColorSelection) {
+  const squares = document.querySelectorAll('.pixel');
+
+  const rainbow = [
+    '#E40303',
+    '#FF8C00',
+    '#FFED00',
+    '#008026',
+    '#24408E',
+    '#732982',
+  ];
+
+  squares.forEach(function (div) {
+    div.addEventListener('mouseover', function () {
+      if (div.classList.contains('isFilled')) {
+        return;
+      }
+      if (userColorSelection === 'rainbow') {
+        div.style.backgroundColor =
+          rainbow[Math.floor(Math.random() * rainbow.length)];
+        div.classList.add('isFilled');
+      } else {
+        div.style.backgroundColor = `${userColorSelection}`;
+        div.classList.add('isFilled');
+      }
+    });
   });
 }
+
+const slider = document.querySelector('.slider');
+const sliderstep = (document.querySelector('.slider').step = '16');
+const output = document.querySelector('.slider-value');
+
+slider.oninput = () => {
+  output.innerHTML = `${slider.value} x ${slider.value} `;
+  createGrid(slider.value);
+  paintEmptySquares(userColorSelection);
+};
 
 window.onload = () => {
   output.innerHTML = `${slider.value} x ${slider.value} `;
   createGrid(slider.value);
+  paintEmptySquares(userColorSelection);
 };
